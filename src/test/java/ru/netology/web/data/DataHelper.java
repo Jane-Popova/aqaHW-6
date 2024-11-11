bc3b86ccb28432f8980416c95ca0fcedb7da5a53
Evgenia Popova <jane477@yandex.ru>
2024-11-11 15:55:01 +0300

    added testValidTransfer

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -2,6 +2,8 @@ package ru.netology.web.data;
 
 import lombok.Value;
 
+import java.util.Random;
+
 public class DataHelper {
     private DataHelper() {
     }
@@ -16,16 +18,37 @@ public class DataHelper {
         return new AuthInfo("vasya", "qwerty123");
     }
 
-    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
-        return new AuthInfo("petya", "123qwerty");
+
+    public static VerificationCode getVerificationCode() {
+        return new VerificationCode("12345");
+    }
+
+    public static CardInfo getFirstCardInfo() {
+        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static CardInfo getSecondCardInfo() {
+        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
+
+    public static int generateRndValidAmount(int amount) {
+        return new Random().nextInt(amount) + 1;
+    }
+
+    public static int generateRndInvalidAmount(int amount) {
+        return Math.abs(amount) + new Random().nextInt(10000);
     }
 
     @Value
     public static class VerificationCode {
-        private String code;
+        String code;
     }
 
-    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
-        return new VerificationCode("12345");
+    @Value
+    public static class CardInfo {
+        String cardNumber;
+        String testId;
     }
+
 }
+
